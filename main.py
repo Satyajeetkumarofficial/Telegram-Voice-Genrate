b865155deb5c4d9175843e397d84af3da046fa3e
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-04-30 13:14:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import os
 import telebot
 import requests
 from dotenv import load_dotenv
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive", 200
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
 load_dotenv()
 
